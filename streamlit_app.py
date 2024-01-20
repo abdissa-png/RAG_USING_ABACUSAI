34e93e6f556eeeac31b57941d70cb7f3318b1aa5
abdissa-png <abdissadegefu1@gmail.com>
2024-01-20 19:26:12 +0300

    file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,46 @@
+
 import streamlit as st
-from abacusai import PredictionClient
+# from abacusai import PredictionClient
 import os
-client = PredictionClient()
+# client = PredictionClient()
+import requests
 
 st.set_page_config(page_title="Simple API Request UI")
 
 text_input = st.text_input('Enter your text:')
 response=""
 if st.button('Submit'):
-    response = client.get_chat_response(
-        deployment_token=os.environ['deployment_token'], 
-        deployment_id=os.environ['deployment_id'], 
-        messages=[{"is_user": True, "text": text_input}], 
-        llm_name=None, 
-        num_completion_tokens=None, 
-        system_message=None, 
-        temperature=0.0, 
-        filter_key_values=None, 
-        search_score_cutoff=None, 
-        chat_config=None, 
-        ignore_documents=False
-    )["messages"][1]["text"]
+    # response = client.get_chat_response(
+    #     deployment_token=os.environ['deployment_token'], 
+    #     deployment_id=os.environ['deployment_id'], 
+    #     messages=[{"is_user": True, "text": text_input}], 
+    #     llm_name=None, 
+    #     num_completion_tokens=None, 
+    #     system_message=None, 
+    #     temperature=0.0, 
+    #     filter_key_values=None, 
+    #     search_score_cutoff=None, 
+    #     chat_config=None, 
+    #     ignore_documents=False
+    # )["messages"][1]["text"]
+    url = f"https://abdissa-degefu.api.abacus.ai/api/getChatResponse?deploymentToken={os.environ['deployment_token']}&deploymentId={['deployment_id']}"
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "messages": [{"is_user":True,"text":text_input}],
+        "llmName": None,
+        "numCompletionTokens": None,
+        "systemMessage": None,
+        "temperature": 0.0,
+        "filterKeyValues": None,
+        "searchScoreCutoff": None,
+        "chatConfig": None,
+        "ignoreDocuments": False
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    response=response.json()["messages"][1]["text"]
+
 # Check if 'previous_conversations' exists in session state
 if 'previous_conversations' not in st.session_state:
     st.session_state.previous_conversations = []
